52ccf5436c5502f1f9a368025b00586cd3ac4375
Sowmya Krishnan <113003820+Sowmya-Krish@users.noreply.github.com>
2024-01-08 14:08:59 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
+            src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201909/contact-details-3381225_1280.jpeg"
             alt="contactus"
             style={{ width: "100%" }}
           />
